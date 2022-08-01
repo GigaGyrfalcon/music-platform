ad9ac42259749346495b75a022fff4f7469e4bac
Giga Songulashvili <giga.songulashvili@gmail.com>
2022-07-31 23:18:08 -0400

    Music platform setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import './App.scss'
+import Activate from './components/pages/Activate'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Music Platform</h1>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">About</Link>
+              </li>
+              <li>
+                <Link to="/register">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/activate" element={<Activate />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/pages/Activate.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Activate() {
+  return (
+    <div className="activate">
+      <h1>Activate</h1>
+    </div>
+  )
+}
+
+export default Activate
